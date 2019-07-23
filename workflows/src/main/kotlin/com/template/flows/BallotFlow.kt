355a357951517d27f96153e5c75146d255e4c6b8
Isaiah Garlin <isaiahgarlin@MacBook-Dev-10.local>
2019-07-23 11:34:47 -0400

    Added to BallotFlow IssueBallotResponder flow, FillBallot Flow, and FillBallotFlowRepsonder I previously made the mistake of combining the IssueBallotResponder and FillBallot flow into one which would make FillBallotFlow unable to be StartableByRPC making it unable to recieve a ballot as input.  I also decided to put the requireThat test for the FillBallot command into the FillBallotFlow responder so the checkTransaction function within that class checks the validity of a filled out ballot.

--- workflows/src/main/kotlin/com/template/flows/BallotFlow.kt
+++ workflows/src/main/kotlin/com/template/flows/BallotFlow.kt
@@ -4,10 +4,15 @@ import co.paralleluniverse.fibers.Suspendable
 import com.example.contract.BALLOTContract
 import com.example.state.BALLOTState
 import net.corda.core.contracts.Command
+import net.corda.core.contracts.Requirements.using
+import net.corda.core.contracts.TimeWindow
 import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
+import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.days
+import java.time.Instant
 
 
 /**
@@ -34,8 +39,11 @@ class IssueBallotFlow(val ballot: BALLOTState) : FlowLogic<SignedTransaction>() 
     override fun call(): SignedTransaction {
 
 
+
         // retrieves the first notary from the network
         val notary = serviceHub.networkMapCache.notaryIdentities.first()
+
+
 
 
         // creates a command for the transaction construction
@@ -45,6 +53,11 @@ class IssueBallotFlow(val ballot: BALLOTState) : FlowLogic<SignedTransaction>() 
 
         builder.addOutputState(ballot, BALLOTContract.BALLOT_CONTRACT_ID)
         builder.addCommand(issueCommand)
+
+
+        val timeWindow: TimeWindow = TimeWindow.withTolerance(serviceHub.clock.instant(), ballot.daysOpen)
+
+        builder.setTimeWindow(timeWindow)
 
 
         // initial signing of the transaction before being sent to
@@ -61,17 +74,19 @@ class IssueBallotFlow(val ballot: BALLOTState) : FlowLogic<SignedTransaction>() 
         val stx = subFlow(CollectSignaturesFlow(ptx, sessions))
 
 
+
         return subFlow(FinalityFlow(stx, sessions))
-
-
 
     }
 }
 
 
 
+
+
+
 @InitiatedBy(IssueBallotFlow::class)
-class FillBallotFlow(val flowSession: FlowSession) : FlowLogic<SignedTransaction>() {
+class IssueBallotResponderFlow(val flowSession: FlowSession) : FlowLogic<SignedTransaction>() {
 
 
     @Suspendable
@@ -83,8 +98,10 @@ class FillBallotFlow(val flowSession: FlowSession) : FlowLogic<SignedTransaction
 
                 val output = stx.tx.outputs.single().data
                 "This must be a BALLOT transaction" using (output is BALLOTState)
-            }
 
+                val ballot = (output as BALLOTState)
+
+            }
         }
 
         val txId = subFlow(stf).id
@@ -93,3 +110,97 @@ class FillBallotFlow(val flowSession: FlowSession) : FlowLogic<SignedTransaction
     }
 
 }
+
+
+
+
+
+@InitiatingFlow
+@StartableByRPC
+class FillBallotFlow(val ballot: BALLOTState, val selected: Map<String,Boolean>) : FlowLogic<SignedTransaction>() {
+
+
+
+    override fun call(): SignedTransaction {
+
+
+        // iterates through selected map
+        // and increases the values in count map by 1
+        // for each selection that has a value of true
+        for ((key, value) in selected) {
+
+            if (value) {
+
+                var initialVal = (ballot.count.get(key) as Int)
+                ballot.count.replace(key,initialVal,initialVal+1)
+            }
+
+        }
+
+        // creates a new transaction with the selections
+        // of the voting party now being used to construct
+        // a new ballot
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+        val newBallot = ballot.copy(selections = selected)
+        val command = Command(BALLOTContract.Commands.FillOut(), ballot.participants.map {it.owningKey})
+
+        val builder = TransactionBuilder(notary=notary)
+
+        builder.addCommand(command)
+        builder.addOutputState(newBallot,BALLOTContract.BALLOT_CONTRACT_ID)
+
+        val ptx = serviceHub.signInitialTransaction(builder)
+
+        val sessions = (ballot.participants - ourIdentity).map {initiateFlow(it)}.toSet()
+        val stx = subFlow(CollectSignaturesFlow(ptx, sessions))
+
+        return subFlow(FinalityFlow(stx, sessions))
+
+
+
+    }
+
+
+}
+
+
+
+@InitiatedBy(FillBallotFlow::class)
+class FillBallotFlowResponder(val flowSession: FlowSession) : FlowLogic<SignedTransaction>() {
+
+
+    override fun call(): SignedTransaction {
+
+        val stf = object : SignTransactionFlow(flowSession) {
+
+            override fun checkTransaction(stx: SignedTransaction) {
+
+                val output = stx.tx.outputs.single().data
+                "This must be a BALLOT transaction" using (output is BALLOTState)
+
+
+                val ballot = output as BALLOTState
+                var count = 0
+
+                for ((key, value) in ballot.selections) {
+
+                    if (value) {
+                        count++
+                    }
+                }
+
+                "Voting party has not chosen more selections than is allowed by issuer." using (count <= ballot.maxChoices)
+            }
+        }
+        val txId = subFlow(stf).id
+
+        return subFlow(ReceiveFinalityFlow(flowSession,expectedTxId = txId))
+
+
+    }
+
+
+
+}
+
+
